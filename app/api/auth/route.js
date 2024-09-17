c4bc83fc8b9bfb0331d1bd9c3f18fca1287edbb6
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-09-17 12:31:39 +0200

    added authentication

---
+++ app/api/auth/route.js
@@ -0,0 +1,32 @@
+import connectdb from "@/mongodb"
+import { NextResponse } from "next/server"
+import User from "@/app/models/user"
+
+export async function POST(request) {
+    const {phonenumber, password} = await request.json()
+    try {
+        const user = await User.findOne({phonenumber})
+        if(!user){
+            return NextResponse.json({
+                success:false,
+                message:"User not found"
+            }) 
+        }
+        if(user.password !== password){
+            return NextResponse.json({
+                success:false,
+                message:"Wrong passowrd"
+            })
+        }
+        return NextResponse.json({
+            success:true,
+            message:"Logged in Successfully"
+        })
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({
+            success:false,
+            message:"Error loging in user"
+        })
+    }
+}
--- app/auth/login/page.js
+++ app/auth/login/page.js
@@ -4,23 +4,43 @@ import { useRouter } from "next/navigation"
 
 export default function Login(){
     const router = useRouter()
-    const [email, setEmail] = useState()
+    const [phonenumber, setPhonenumber] = useState()
     const [password, setPassword] = useState()
+    const [isLoading, setIsLoading] = useState(false)
 
-    const login = (e) => {
+    const login = async(e) => {
         e.preventDefault();
-        router.push("/dashboard")
+        setIsLoading(true)
+        const response = await fetch("/api/auth",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({
+                phonenumber,
+                password
+            })
+        })
+        const data  = await response.json()
+        if(data.success){
+            setIsLoading(false)
+            router.push("/dashboard")
+        }else{
+            setIsLoading(false)
+            alert(data.message)
+        }
+        
     }
+
     return(
         <div className="w-full h-screen flex items-center justify-center px-16">
             <form className="flex flex-col gap-2 bg-gray-200 p-4 rounded shadow w-full md:w-1/4" onSubmit={(e) => login(e)}>
                 <h1 className="font-semibold text-xl text-gray-900 ">Login</h1>
                 <input className="border border-gray-900 rounded w-full bg-transparent p-2 text-sm"
-                    placeholder="email"
-                    onChange={(e)=>setEmail(e.target.value)} 
+                    placeholder="phone number"
+                    onChange={(e)=>setPhonenumber(e.target.value)} 
                     required/>
                 <input className="border border-gray-900  rounded w-full bg-transparent p-2 text-sm"
-                    placeholder="email"
+                    placeholder="password"
+                    type="password"
                     onChange={(e)=>setPassword(e.target.value)} 
                     required/>
                 <button className="bg-gray-900 text-white p-2 rounded text-sm" type="submit">Login</button>
