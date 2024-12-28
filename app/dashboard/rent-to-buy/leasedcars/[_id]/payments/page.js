05ac4b3c73914ff90ff5eeb1d008d30c4c6f2251
ElvinAnesu <163331191+ElvinAnesu@users.noreply.github.com>
2024-12-28 23:04:51 +0200

    feat:payments appearing on payments list

--- app/dashboard/rent-to-buy/leasedcars/[_id]/payments/page.js
+++ app/dashboard/rent-to-buy/leasedcars/[_id]/payments/page.js
@@ -2,47 +2,113 @@
 import BreadCrumb from "@/app/components/dashboard/common/breadcrumb";
 import { updateDownPayment } from "@/lib/server-actions/lease";
 import { use, useState } from "react";
+import {
+	UserCircleIcon,
+	CurrencyDollarIcon,
+	CalendarDaysIcon,
+	DocumentTextIcon,
+} from "@heroicons/react/24/outline";
 
-export default function LeasedCarDetails({ params }) {
+
+export default function LeasedCarDetails({ params }) { 
+
 	const { _id } = use(params);
-    const [amount, setAmount] = useState(0)
-    const [isLoading, setIsLoading] = useState(false)
+	const [isLoading, setIsLoading] = useState(false) 
+	const [fullname, setFullname] = useState();
+	const [date, setDate] = useState();
+	const [amount, setAmount] = useState();
+	const [description, setDescription] = useState();
+	const [paymentMethod, setPaymentMethod] = useState();
 
-    const _updatePayments = async () => {
+
+	const _updatePayments = async (e) => { 
+		e.preventDefault()
         setIsLoading(true)
-        await updateDownPayment(_id, amount);
+        await updateDownPayment(
+					_id,
+					amount,
+					fullname,
+					date,
+					amount,
+					description,
+					paymentMethod
+				);
         setIsLoading(false)
     }
 
+	
 
 	return (
 		<div className="flex flex-col gap-8">
 			<BreadCrumb title={"Update Payments"} />
-			<div className="bg-white rounded shadow grid grid-cols-1 md:grid-cols-4 p-4 gap-4">
-				<div className="w-full">
-					<h5 className="text-sm">Date</h5>
-					<input
-						className="w-full border border-gray-300 rounded h-10 bg-gray-300 px-2 text-sm font-semibold"
-						placeholder="first name"
-						name="date"
-						type="date"
-					/>
+			<div className="w-full flex flex-col">
+				<div className="flex w-full bg-white rounded-lg p-2">
+					<form
+						className="flex flex-col w-full gap-4 bg-gray-200 rounded p-4"
+						onSubmit={(e) => _updatePayments(e)}
+					>
+						<div>
+							<h1 className="text-xs font-semibold">Full Name</h1>
+							<div className="relative">
+								<input
+									className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+									placeholder="description"
+									onChange={(e) => setFullname(e.target.value)}
+								/>
+								<UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+							</div>
+						</div>
+						<div>
+							<h1 className="text-xs font-semibold">Payment Description</h1>
+							<div className="relative">
+								<input
+									className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+									placeholder="description"
+									onChange={(e) => setDescription(e.target.value)}
+								/>
+								<DocumentTextIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+							</div>
+						</div>
+						<div>
+							<h1 className="text-xs font-semibold">Amount</h1>
+							<div className="relative">
+								<input
+									className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+									placeholder="amount"
+									type="number"
+									onChange={(e) => setAmount(e.target.value)}
+								/>
+								<CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+							</div>
+						</div>
+						<div className="flex flex-col">
+							<h1 className="text-xs font-semibold">Payment Method</h1>
+							<div className="relative">
+								<input
+									className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+									placeholder="amount"
+									onChange={(e) => setPaymentMethod(e.target.value)}
+								/>
+								<CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+							</div>
+						</div>
+						<div className="flex flex-col">
+							<h1 className="text-xs font-semibold">Date</h1>
+							<div className="relative">
+								<input
+									className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+									placeholder="amount"
+									type="date"
+									onChange={(e) => setDate(e.target.value)}
+								/>
+								<CalendarDaysIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+							</div>
+						</div>
+						<button className="bg-purple-900 rounded p-2 text-white max-w-32">
+							Submit
+						</button>
+					</form>
 				</div>
-				<div className="w-full">
-					<h5 className="text-sm">Surname</h5>
-					<input
-						className="w-full border border-gray-300 rounded h-10 bg-gray-300 px-2 text-sm font-semibold"
-						placeholder="amount"
-						name="amount"
-                        type="number"
-                        onChange={(e)=> setAmount(e.target.value)}
-					/>
-                </div>
-                <div className="hidden md:block md:col-span-2"></div>
-                <button className="bg-purple-900 rounded text-white p-2"
-                     onClick={_updatePayments}>
-					{isLoading ? "loading" : "Next"}
-				</button>
 			</div>
 		</div>
 	);
--- app/models/lease.js
+++ app/models/lease.js
@@ -1,54 +1,58 @@
 import mongoose, { Schema } from "mongoose";
 
-const leaseSchema = new Schema({
-	clientName: {
-		type: String,
-		required: true,
+const leaseSchema = new Schema(
+	{
+		clientName: {
+			type: String,
+			required: true,
+		},
+		clientSurname: {
+			type: String,
+			required: true,
+		},
+		clientIdNo: {
+			type: String,
+			required: true,
+		},
+		clientEmail: {
+			type: String,
+		},
+		clientPhonenumber: {
+			type: String,
+			required: true,
+		},
+		clientAddress: {
+			type: String,
+			required: true,
+		},
+		leasedCar: {
+			type: String,
+		},
+		totalPrice: {
+			type: Number,
+			default: 0,
+		},
+		monthlyPayments: {
+			type: Number,
+			default: 0,
+		},
+		leaseTenure: {
+			type: String,
+		},
+		downPayment: {
+			type: Number,
+			default: 0,
+		},
+		startDate: {
+			type: Date,
+		},
+		endDate: {
+			type: Date,
+		},
+		documents: [{}],
 	},
-	clientSurname: {
-		type: String,
-		required: true,
-	},
-	clientIdNo: {
-		type: String,
-		required: true,
-	},
-	clientEmail: {
-		type: String,
-	},
-	clientPhonenumber: {
-		type: String,
-		required: true,
-	},
-	clientAddress: {
-		type: String,
-		required: true,
-	},
-	leasedCar: {
-		type: String,
-	},
-	totalPrice: {
-		type: Number,
-	},
-	monthlyPayments: {
-		type: String,
-	},
-	leaseTenure: {
-		type: String,
-	},
-	downPayment: {
-		type: Number,
-	},
-	startDate: {
-		type: Date,
-    },
-    endDate: {
-        type:Date
-    },
-    documents: [{
-       
-    }],
-}, { timestamps: true });
+	{ timestamps: true }
+);
 
 const Lease = mongoose.models.Lease || mongoose.model("Lease", leaseSchema);
 export default Lease;
